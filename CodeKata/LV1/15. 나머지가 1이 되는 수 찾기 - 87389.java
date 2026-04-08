389763a16e22163a3b3e208cd21f51e7f6b58e72
GolemOnce <shh3350@gmail.com>
2026-04-08 11:12:55 +0900

    15. 87389 : [월간 코드 챌린지 시즌3] 나머지가 1이 되는 수 찾기

---
+++ CodeKata/LV1/15. 나머지가 1이 되는 수 찾기 - 87389.java
@@ -0,0 +1,11 @@
+/* [월간 코드 챌린지 시즌3] 나머지가 1이 되는 수 찾기 */
+
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        for (int i = 1; i < n; i++) {
+            if (n % i == 1) { return i; }
+        }
+        return answer;
+    }
+}
