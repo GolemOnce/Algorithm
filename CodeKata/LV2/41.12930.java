bb478737326579371c3621a93657aa6a60134cd4
GolemOnce <shh3350@gmail.com>
2026-04-23 11:00:00 +0900

    41. 12930 : [연습문제] 이상한 문자 만들기 - Character.toUpperCase(), toLowerCase()

---
+++ CodeKata/LV2/41.12930.java
@@ -0,0 +1,21 @@
+package CodeKata.LV2;
+
+/* [연습문제] 이상한 문자 만들기 */
+
+class Solution {
+    public String solution(String s) {
+        StringBuilder sb = new StringBuilder();
+        int idx = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == ' ') {
+                sb.append(" ");
+                idx = 0;
+            } else {
+                sb.append(idx % 2 == 0 ? Character.toUpperCase(s.charAt(i)) : Character.toLowerCase(s.charAt(i)));
+                idx += 1;
+            }
+            System.out.println(sb.toString());
+        }
+        return sb.toString();
+    }
+}
